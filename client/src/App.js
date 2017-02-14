080f96f235db0a31a859894bdd583588173e7fc7
zhaozhuoboy <zhaozhuoboy@163.com>
2017-02-14 13:44:16 +0800

    layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
 //admin 后台管理界面
 import Header from './share/header/Header';
-import LeftNav from './components/leftnav/LeftNav'
+import LeftNav from './components/leftnav/LeftNav';
+
 
 
 class App extends React.Component {
 
   render () {
+    let styles={
+      root:{
+        width:"100%",
+        height:"100vh"
+      }
+    }
     return(
-      <div>
+      <div style={styles.root}>
         <Header />
         <div className="main-container">
           <div className="left-nav">
             <LeftNav />
           </div>
-          <div className="right-content"></div>
+          <div className="right-content" key='1'>
+            { this.props.children }adfadsf
+          </div>
         </div>
       </div>
     )
--- client/src/components/leftnav/LeftNav.js
+++ client/src/components/leftnav/LeftNav.js
@@ -19,38 +19,30 @@ const LeftNav = React.createClass({
     return (
       <QueueAnim duration={1000} type='left' interval={0}>
       <Menu onClick={this.handleClick}
-        style={{ width: 240 }}
-        defaultOpenKeys={['sub1']}
+        style={{height:"100vh"}}
+        defaultOpenKeys={['sub1','sub2','sub3']}
         selectedKeys={[this.state.current]}
         mode="inline"
         key='1'
       >
-        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-          <MenuItemGroup title="Item 1">
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-          </MenuItemGroup>
-          <MenuItemGroup title="Item 2">
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
-          </MenuItemGroup>
+        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>员工信息管理</span></span>}>
+            <Menu.Item key="1">查看所有员工</Menu.Item>
+            <Menu.Item key="2">添加员工信息</Menu.Item>
+            <Menu.Item key="3">修改员工信息</Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
-          <Menu.Item key="5">Option 5</Menu.Item>
-          <Menu.Item key="6">Option 6</Menu.Item>
-          <SubMenu key="sub3" title="Submenu">
-            <Menu.Item key="7">Option 7</Menu.Item>
-            <Menu.Item key="8">Option 8</Menu.Item>
-          </SubMenu>
+        <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>汽车信息管理</span></span>}>
+          <Menu.Item key="4">查看所有汽车</Menu.Item>
+          <Menu.Item key="5">添加新款汽车</Menu.Item>
+          <Menu.Item key="6">修改汽车信息</Menu.Item>
         </SubMenu>
-        <SubMenu key="sub4" title={<span><Icon type="setting" /><span>Navigation Three</span></span>}>
-          <Menu.Item key="9">Option 9</Menu.Item>
-          <Menu.Item key="10">Option 10</Menu.Item>
-          <Menu.Item key="11">Option 11</Menu.Item>
-          <Menu.Item key="12">Option 12</Menu.Item>
+        <SubMenu key="sub3" title={<span><Icon type="setting" /><span>新闻/图片发布</span></span>}>
+          <Menu.Item key="7">新闻发布</Menu.Item>
+          <Menu.Item key="8">图片发布</Menu.Item>
+
         </SubMenu>
       </Menu>
       </QueueAnim>
+
     );
   },
 });
