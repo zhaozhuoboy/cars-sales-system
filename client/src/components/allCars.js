709d8b6c02b49c969ce9ed5a2c3fe8f51fe7aaac
zhaozhuoboy <zhaozhuoboy@163.com>
2017-02-26 13:58:48 +0800

    正在添加分页功能

--- client/src/components/allCars.js
+++ client/src/components/allCars.js
@@ -1,26 +1,37 @@
 import React, { PropTypes } from 'react'
 import AddCar from './qicheguanli/addCar'
 import CarItem from './qicheguanli/carItem'
-import axios from 'axios'
-import { notification } from 'antd'
-import SiteConfig from '../config'
+import axios from 'axios';
+import isEmpty from 'lodash/fp/isEmpty';
+import { notification ,Pagination ,Table, Icon  } from 'antd'
+import SiteConfig from '../config';
+
 class AllCars extends React.Component {
   constructor(){
     super()
     this.state={
-      allCarData:[],
-      loadData:true
+      allCarData:[],//所有的数据
+      loadData:true,
+      pageSize:10,//显示几条
+      pageNum:1
     }
   }
+  onChange(page){
+    this.setState({
+      pageNum: page
+    });
+  }
+//分页功能，初始化时 将 第1页  1  和显示条数  10  提交到后台，
+//后台查询  跳过 1*10-10 之后的10条 skip(pageNum*pageSize-pageSize).limit(10)
   componentWillMount(){
     axios.get(`${SiteConfig.host}/getallcars`)
          .then((res)=>{
-           console.log(res.data);
            this.setState({
              allCarData:res.data.cars,
-             loadData:false
+             loadData:false,
            });
          })
+
   }
   del(car_id){
     axios.delete(`${SiteConfig.host}/delcar/${car_id}`)
@@ -38,7 +49,7 @@ class AllCars extends React.Component {
          })
   }
   render () {
-    const allcars = this.state.allCarData.map( (item , i) => <CarItem
+    var allcars = this.state.allCarData.map( (item , i) => <CarItem
     key={item._id}
     id={item._id}
     name={item.name}
@@ -66,6 +77,10 @@ class AllCars extends React.Component {
             </tbody>
           </table>
         </div>
+        <Pagination current={this.state.current}
+            defaultCurrent={1}
+           onChange={this.onChange.bind(this)} total={this.state.allCarData.length} />
+
       </div>
     )
   }
--- client/src/components/leftnav/LeftNav.js
+++ client/src/components/leftnav/LeftNav.js
@@ -16,6 +16,21 @@ const LeftNav = React.createClass({
       current: e.key,
     });
   },
+  setNavBarState(){
+    this.setState({
+      current:this.context.router.isActive('/all-yuangong',true) ? "1":
+              this.context.router.isActive('/all-cars') ? "2":
+              this.context.router.isActive('/edit-cars') ? "3":
+              this.context.router.isActive('/news-publish') ? "4":
+              this.context.router.isActive('/pic-publish') ? "5":''
+    })
+  },
+  componentWillMount(){
+    this.setNavBarState();
+  },
+  componentWillReceiveProps(nextProps){
+    this.setNavBarState();
+  },
   render() {
     return (
       <QueueAnim duration={1000} type='left' interval={0}>
@@ -31,12 +46,11 @@ const LeftNav = React.createClass({
         </SubMenu>
         <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>汽车信息管理</span></span>}>
           <Menu.Item key="2"><Link to='all-cars'>查看所有汽车</Link></Menu.Item>
-          <Menu.Item key="3"><Link to='add-cars'>添加新款汽车</Link></Menu.Item>
-          <Menu.Item key="4"><Link to='edit-cars'>修改汽车信息</Link></Menu.Item>
+          <Menu.Item key="3"><Link to='edit-cars'>修改汽车信息</Link></Menu.Item>
         </SubMenu>
         <SubMenu key="sub3" title={<span><Icon type="setting" /><span>新闻/图片发布</span></span>}>
-          <Menu.Item key="5"><Link to='news-publish'>新闻发布</Link></Menu.Item>
-          <Menu.Item key="6"><Link to='pic-publish'>图片发布</Link></Menu.Item>
+          <Menu.Item key="4"><Link to='news-publish'>新闻发布</Link></Menu.Item>
+          <Menu.Item key="5"><Link to='pic-publish'>图片发布</Link></Menu.Item>
 
         </SubMenu>
       </Menu>
@@ -45,4 +59,7 @@ const LeftNav = React.createClass({
     );
   },
 });
+LeftNav.contextTypes={
+  router:React.PropTypes.object.isRequired,
+}
 export default LeftNav;
